8db985d0da99987efeef1d575604d248e25faf18
pavanmalem <87222061+pavanmalem@users.noreply.github.com>
2025-06-12 19:52:38 +0530

    Add files via upload

---
+++ Area_of_Circle.java
@@ -0,0 +1,12 @@
+
+
+public class Area_of_Circle {
+    public static void main(String[] args) {
+        double a= 3.14;
+        int Radius= 10;
+        double Area = a * Radius * Radius;
+        System.out.println("The area of the circle is:"+ Area);
+        
+
+    }
+}
